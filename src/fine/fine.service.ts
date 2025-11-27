490b57ddfd57f31a15cea8ce791a47b234c21adf
ferdus615 <ferdus615@gmail.com>
2025-11-27 21:03:49 +0600

    added all the fine dtos

--- src/fine/fine.service.ts
+++ src/fine/fine.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Fine } from './entities/fine.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Loan } from 'src/loan/entities/loan.entity';
+import { CreateFineDto } from './dto/createFineDto.dto';
+import { ResponseFineDto } from './dto/responseFineDto.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
-export class FineService {}
+export class FineService {
+  constructor(
+    @InjectRepository(Fine) private readonly fineRepository: Repository<Fine>,
+    @InjectRepository(User) private readonly userRepositoty: Repository<User>,
+    @InjectRepository(Loan) private readonly loanRepository: Repository<Loan>,
+  ) {}
+
+  async createFine(dto: CreateFineDto): Promise<ResponseFineDto> {
+    const user = await this.userRepositoty.findOne({
+      where: { id: dto.user_id },
+    });
+    if (!user) throw new NotFoundException(`User not found!`);
+
+    const loan = await this.loanRepository.findOne({
+      where: { id: dto.loan_id },
+    });
+    if (!loan) throw new NotFoundException(`Loan not found!`);
+  }
+}
