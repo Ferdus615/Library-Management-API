66d0bc2f7ff14ed9ec30044e5bb6e06481a831ac
ferdus615 <ferdus615@gmail.com>
2025-12-16 18:31:48 +0600

    added the create route

--- src/fine/fine.controller.ts
+++ src/fine/fine.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { FineService } from './fine.service';
+import { CreateFineDto } from './dto/createFineDto.dto';
+import { ResponseFineDto } from './dto/responseFineDto.dto';
 
 @Controller('fine')
 export class FineController {
   constructor(private readonly fineService: FineService) {}
+
+  @Post()
+  async createFine(@Body() dto: CreateFineDto): Promise<ResponseFineDto> {
+    return await this.fineService.createFine(dto);
+  }
+
+  @
 }
