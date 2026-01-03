80e912c7d414e77d85936f027bfc0ca4a89236ee
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-01-03 22:01:58 +0600

    added the create reservation controller

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
+import { CreateReservationDto } from './dto/createReservationDto.dto';
+import { ResponseReservationDto } from './dto/rseponseReservationDto.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
+
+  @Post()
+  async createReservation(
+    @Body() dto: CreateReservationDto,
+  ): Promise<ResponseReservationDto> {
+    const reservation = await this.reservationService.createReservation(dto);
+    return plainToInstance(ResponseReservationDto, reservation, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
