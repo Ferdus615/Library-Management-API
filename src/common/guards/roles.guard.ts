3ff2d5bcd45c176fa256e39a4f7700efadba5b41
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-02-01 14:35:14 +0600

    fix role guard metadata resolution bug letting any loged in user successfully hit any api

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -7,17 +7,22 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.get<MemberStatus[]>(
+    const requiredRoles = this.reflector.getAllAndOverride<MemberStatus[]>(
       'roles',
-      context.getHandler(),
+      [context.getHandler(), context.getClass()],
     );
 
-    if (!requiredRoles) return true;
+    if (!requiredRoles || requiredRoles.length === 0) return true;
 
     const rerquest = context.switchToHttp().getRequest();
-    const user = rerquest.user as { role: MemberStatus };
+    const user = rerquest.user as { role?: MemberStatus };
 
     if (!user || !user.role) return false;
+
+    console.log({
+      requiredRoles,
+      userRole: user?.role,
+    });
 
     return requiredRoles.includes(user.role);
   }
