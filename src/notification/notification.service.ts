98185cecb1622a92f89be1b2cfabc6a2716574ec
ferdus615 <ferdus615@gmail.com>
2026-02-03 22:07:59 +0600

    updated the notifiacation module to have import & export the module and add to the database.

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateNotificationDto } from './dto/create-notification.dto';
-import { UpdateNotificationDto } from './dto/update-notification.dto';
 
 @Injectable()
 export class NotificationService {}
