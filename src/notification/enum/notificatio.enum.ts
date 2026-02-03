c8e873dac948acfa9ccda85851c594f29f030ab7
ferdus615 <ferdus615@gmail.com>
2026-02-03 20:16:30 +0600

    added the notification enum

---
+++ src/notification/enum/notificatio.enum.ts
@@ -0,0 +1,7 @@
+export enum NotificationType {
+  RESERVATION_READY = 'reservation_ready',
+  RESERVATION_EXPIRED = 'reservation_expired',
+  LOAN_RETURNED = 'loan_returned',
+  LOAN_OVERDUE = 'loan_overdue',
+  FINE_CREATED = 'fine_created',
+}
