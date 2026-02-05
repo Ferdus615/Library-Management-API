837f19094bf2451c6ec7838f1e3abef48c06e957
ferdus615 <ferdus615@gmail.com>
2026-02-05 17:02:43 +0600

    refactor the auth module codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/loginDto.dto';
 import { AuthResponseDto } from './dto/responseAuthDto.dto';
-import { plainToInstance } from 'class-transformer';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Public } from './decorators/public.decorators';
 
@@ -29,10 +28,6 @@ export class AuthController {
     const isUser = await this.authService.validateUser(dto);
     if (!isUser) throw new UnauthorizedException('Invalid email or password');
 
-    const authenticatedUser = await this.authService.login(isUser);
-
-    return plainToInstance(AuthResponseDto, authenticatedUser, {
-      excludeExtraneousValues: true,
-    });
+    return await this.authService.login(isUser);
   }
 }
