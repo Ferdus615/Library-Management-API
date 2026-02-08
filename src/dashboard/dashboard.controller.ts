3450bd82187c691349f948345e45b6ee93f2007e
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-02-08 14:44:15 +0600

    fixed and added the documentation to the dashboard data.

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -5,12 +5,31 @@ import { Roles } from 'src/common/decorators/roles.decorator';
 import { MemberStatus } from 'src/user/enum/member.enum';
 import { AdminDashboardDto } from './dto/adminDashboardDto.dto';
 import { MemberDashboardDto } from './dto/memberDashboardDto.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Dashboard')
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('admin')
+  @ApiOperation({
+    summary: 'Get admin dashboard',
+    description: 'Returns admin dashboard data',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Admin dashboard data',
+    type: AdminDashboardDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
   @UseGuards(RolesGuard)
   @Roles(MemberStatus.ADMIN, MemberStatus.LIBRARIAN)
   async getAdminDashboard(): Promise<AdminDashboardDto> {
@@ -18,6 +37,25 @@ export class DashboardController {
   }
 
   @Get('member')
+  @ApiOperation({
+    summary: 'Get member dashboard',
+    description: 'Returns member dashboard data',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Member dashboard data',
+    type: MemberDashboardDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @UseGuards(RolesGuard)
+  @Roles(MemberStatus.MEMBER, MemberStatus.LIBRARIAN, MemberStatus.ADMIN)
   async getMemberDashboard(@Req() req): Promise<MemberDashboardDto> {
     return await this.dashboardService.getMemberDashboard(req.user.id);
   }
